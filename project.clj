903670f90dcf31b96e05b9676bd13d31c376510f
rubal <rubal@helpshift.com>
2016-07-22 15:43:58 +0530

    Added validation. Checks for unique values of name and email. Adds UUID as a unique id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-q(defproject addressbook "0.1.0-SNAPSHOT"
+(defproject addressbook "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- src/addressbook/core.clj
+++ src/addressbook/core.clj
@@ -1,45 +1,82 @@
 (ns addressbook.core
   (:require [ring.adapter.jetty :as ring]
-            [cheshire.core :as json])
- (:use compojure.core))
+            [cheshire.core :as json]
+            [schema.core :as s :include-macros true])
+  (:use compojure.core)
+  (:import java.util.UUID))
 
 
-(defonce BOOK (atom []))  ;;taking vector to maintain the id order
+(defonce BOOK (atom []))  
 
 
-(defonce id (atom 0))
+(def email-regex #".+\@.+\..+")
+(def phn-regex #"\d\d\d\d\d\d\d\d\d\d")
+(def zip-regex #"\d\d\d\d\d")
+
+
+(def data-schema
+ {(s/required-key :name) s/Str
+  (s/required-key :email) email-regex
+  (s/optional-key :phone) phn-regex
+  (s/optional-key :address) {(s/required-key :house) s/Str
+                             (s/required-key :apartment) s/Str
+                             (s/required-key :city) s/Str
+                             (s/required-key :state) s/Str
+                             (s/required-key :zip) zip-regex
+                             }})
+
 
 
 (defn process-body
-  "The function accepts the body of request as an argument. 
-   This function converts the input request from JSON into a clojure map and returns this map."
+  "Accepts the body of request as an argument and converts the input 
+  request from JSON into a clojure map and returns this map."
   [request]
-  (json/parse-string (slurp (:body request)) true))
+  (s/validate data-schema (json/parse-string (slurp (:body request)) true)))
 
+
+(defn gen-id
+  "Returns a unique id."
+  []
+  (str (UUID/randomUUID)))
+
+(defn get-values-as-map
+  [k v]
+  (let [a (some (fn [x] (if (= (.toLowerCase v) (.toLowerCase (get x k))) x ))@BOOK)]a))
+
+
+(defn exists? [k w]
+  (if (get-values-as-map k w )
+    true
+    false))
 
 (defn enter-data
-  "This function takes a map as an argument and adds a unique ID to it. 
-   It enters this processed argument into the database list and returns the ID of the record."
+  "Takes a map as an argument and adds a unique ID to it and enters it 
+  into the addressbook and returns the ID of the record."
   [request]
-  (swap! id inc)
-  (let [body (merge {:id @id} (process-body request))]
-    (swap! BOOK (fn [m] (merge m body)))
-    (str "ID of the record: " @id)))
-
-
-;;format this
-(defn display-data-handler
-  "A handler to display all the records present in the address book"
-  [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (json/generate-string @BOOK {:pretty true})})
+  (let [id (gen-id)
+        body (merge {:id id} (process-body request))]
+    (if (or (exists? :name (get body :name)) (exists? :email (get body :email)))
+      (str "Value exists!")
+      (swap! BOOK (fn [m] (merge m body))))))
 
 
 
-;;format this
-(defn display
-  "A handler to display all the records present in the address book"
+
+
+(defn get-by-id
+  "Returns a record map matching the id."
+  [id]
+  (let [a (some (fn [x] (if (= id (get x :id)) x)) @BOOK)] a))
+
+
+(defn get-index
+  "Returns the index of the record in the addressbook based on the id in the argument."
+  [id]
+  (.indexOf @BOOK (get-by-id id)) )
+
+
+(defn display-records
+  "Displays all the records present in the address book"
   [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
@@ -47,48 +84,42 @@
 
 
 (defn get-values
-  "Display values of a record based on the key and value provided.  "
+  "Display values of a record based on the key and value provided. 
+  If the value is a name then the first name and last name must be separted by %20"
   [k v]
-  (let [a (some (fn [x] (if (= v (get x k)) x)) @BOOK)]
+  (let [a (some (fn [x] (if (= (.toLowerCase v) (.toLowerCase (get x k))) x)) @BOOK)]
     (json/generate-string a {:pretty true})))
 
 
-(defn get-by-id
-  [id]
-  (let [a (some (fn [x] (if (= id (get x :id)) x)) @BOOK)] a))
-
-
-
-(defn handler [request]
+(defn process-request-handler
+  "Returns the body of the request in the form of a map."
+  [request]
   (process-body request))
 
 
-(defn update-by-index [handler id]
+(defn update-by-id
+  "Updates the record represented by the id provided as an argument."
+  [handler id]
   (fn [request]
-    (let [response (merge {:id (Integer. id)} (handler request))]
+    (let [response (merge {:id id} (process-request-handler request))]
       (reset! BOOK (assoc @BOOK (get-index id) response ))
-      (str "ID" id "updated "))))
+      (str "ID: " id " updated successfully!"))))
 
 
-(defn get-index
-  [id]
-  (.indexOf @BOOK (get-by-id (Integer. id))) )
-
-
-(defn delete-by-index
+(defn delete-by-id
+  "Deletes the record represented by the id provided as an argument."
   [id]
   (let [index (get-index id)]
     (reset! BOOK (vec (concat (subvec @BOOK 0 index) (subvec @BOOK (inc index)))))
-    (str "ID " id "deleted ")))
+    (str "ID: " id " deleted!")))
 
 
-;;change the name
-(defroutes address
-  (GET "/address" [] display)  ;;working
-  (GET "/address/:id" [id] (get-values :id (Integer. id))) ;;working     
-  (GET "/address/search/:name" [name] (get-values :name name)) ;;working
-  (POST "/address" [] enter-data)           ;;working
-  (PUT "/address/:id" [id] (update-by-index handler id))
-  (DELETE "/address/:id" [id] (delete-by-index id)))  ;;working
+(defroutes address-book
+  (GET "/address" [] display-records)  
+  (GET "/address/:id" [id] (get-values :id id))      
+  (GET "/address/search/:name" [name] (get-values :name name))
+  (POST "/address" [] enter-data)           
+  (PUT "/address/:id" [id] (update-by-id process-request-handler id))
+  (DELETE "/address/:id" [id] (delete-by-id id)))  
 
 
