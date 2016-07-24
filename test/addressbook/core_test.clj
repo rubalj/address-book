58f2bb391ed9da7da3a072cba7ab0fe4d82879f3
rubal <rubal@helpshift.com>
2016-07-25 00:58:48 +0530

    Added some trial test cases.

--- test/addressbook/core_test.clj
+++ test/addressbook/core_test.clj
@@ -1,7 +1,30 @@
 (ns addressbook.core-test
   (:require [clojure.test :refer :all]
-            [addressbook.core :refer :all]))
+            [addressbook.core :refer :all]
+            [ring.mock.request :as mock]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+
+(deftest get-values-test
+  (is (= (get-values :id (mock/request :get "2"))
+         {:status  404
+          :headers {"content-type" "text/plain"}
+          :body    "Requested record not found!"})))
+
+
+(deftest delete-by-id-test
+  (is (= (delete-by-id (mock/request :delete "2"))
+         {:status  404
+          :headers {"content-type" "text/plain"}
+          :body    "Requested record does not exist!"})))
+
+
+
+
+
+
+
+
+
+
