1aa53b628d4f5abd881ce705fc08353e53f94495
rubal <rubal@helpshift.com>
2016-07-25 10:53:30 +0530

    Added the missing dependency in project.clj - ring/ring-mock

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
 		[ring "1.5.0"]
 		[cheshire "5.6.3"]
-		[compojure "1.5.1"]
+                [compojure "1.5.1"]
+                [ring/ring-mock "0.3.0"]
 		[prismatic/schema "1.1.2"]]
   :main ^:skip-aot addressbook.core
   :target-path "target/%s"
