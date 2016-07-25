a5d3fdfdb156ba126a22ae7ca0d0422b10543665
rubal <rubal@helpshift.com>
2016-07-25 12:10:34 +0530

    Added new test case for enter data (For reference)

--- test/addressbook/core_test.clj
+++ test/addressbook/core_test.clj
@@ -1,4 +1,4 @@
-(ns addressbook.core-test
+ (ns addressbook.core-test
   (:require [clojure.test :refer :all]
             [addressbook.core :refer :all]
             [ring.mock.request :as mock]))
@@ -12,6 +12,8 @@
           :headers {"content-type" "text/plain"}
           :body    "Requested record not found!"})))
 
+;;id is a string not a number
+
 
 (deftest delete-by-id-test
   (is (= (delete-by-id (mock/request :delete "2"))
@@ -20,11 +22,24 @@
           :body    "Requested record does not exist!"})))
 
 
-
-
-
-
-
+(deftest enter-data-test
+  (let [resp (enter-data (mock/request :post "/address/"  "{ \"name\" : \"rub\",
+ \"email\" : \"rub@helpshift.com\",
+ \"phone\" : \"1656033225\",
+ \"address\" : {
+   \"house\" : \"420\",
+   \"apartment\" : \"Bellview Complex\",
+   \"city\" : \"San Francisco\",
+   \"state\" : \"CA\",
+   \"zip\" : \"94107\"
+ }
+}"))]
+    (is (= 201
+           (:status resp)))
+    (is (= {"content-type" "text/plain"}
+           (:headers resp)))
+    (is (.startsWith (:body resp)
+                     "New record entered with ID: " ))))
 
 
 
